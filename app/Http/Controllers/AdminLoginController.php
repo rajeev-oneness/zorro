1214cbf238d4caa012d0b2c636627f25fbcb69a9
DevArpan7 <72331581+Arpan-Chanda@users.noreply.github.com>
2021-07-02 13:41:54 +0530

    changes

--- app/Http/Controllers/AdminLoginController.php
+++ app/Http/Controllers/AdminLoginController.php
@@ -71,9 +71,9 @@ class AdminLoginController extends Controller
     {
         $deliverStatus = Admin::pluck('delivery_status')->toArray();
         $bookings = Booking::get();
-        $customers = Booking::select('from_customer_id','from_name',\DB::raw('COUNT(bookings.id) as TotalCount'))->orderBy('TotalCount','DESC')->groupBy('bookings.from_customer_id')->limit(5)->get();
+        $customers = Booking::select(\DB::raw('COUNT(bookings.id) as TotalCount'))->orderBy('TotalCount','DESC')->groupBy('bookings.from_customer_id')->limit(5)->get();
         // dd($customers);
-        $drivers = Booking::select('driver_id',\DB::raw('COUNT(bookings.id) as TotalCount'))->orderBy('TotalCount','DESC')->groupBy('bookings.driver_id')->limit(5)->get();
+        $drivers = Booking::select(\DB::raw('COUNT(bookings.id) as TotalCount'))->orderBy('TotalCount','DESC')->groupBy('bookings.driver_id')->limit(5)->get();
         // foreach ($drivers as $key => $value) {
         //     $value->rider;
         // }
